e7f503864ed1908f89338273e006da91e37fd982
xxion <monkey850416@gmail.com>
2021-11-03 15:20:21 +0800

    fix signal by select

--- socket_prac/pthread_socket/server.c
+++ socket_prac/pthread_socket/server.c
@@ -4,17 +4,19 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/select.h>
 #include "singlylinkedlist.c"
 
 #define MAX_CLIENT 10
 #define BUFFER_SIZE 1024
 #define PORT 12345
 
-int cli_count = 0, server_flag = 0, server_socketFD;
+int cli_count = 0, flag = 0, server_socketFD;
 pthread_mutex_t list_mutex = PTHREAD_MUTEX_INITIALIZER;
 Node *head = NULL;
 
@@ -28,53 +30,76 @@ void close_linkedlist()
     }
     freeallNodes(head);
 }
+
+// using fcntl to set FD nonblocking
+void setNonblocking(int fd)
+{
+    int opts;
+    opts = fcntl(fd, F_GETFL);
+    if (opts < 0)
+    {
+        perror("fcntl(sock,GETFL)");
+        return;
+    }
+
+    opts = opts | O_NONBLOCK;
+    if (fcntl(fd, F_SETFL, opts) < 0)
+    {
+        perror("fcntl(sock,SETFL,opts)");
+        return;
+    }
+}
+
 void sig_handler(int a)
 {
-    close_linkedlist();
-    close(server_socketFD);
-    exit(EXIT_FAILURE);
+    //!!
+    flag = 1;
 }
 
 void *client_handler(void *fd)
 {
     int sockfd = *(int *)fd;
+    ssize_t n;
     char recv_buff[BUFFER_SIZE];
     memset(recv_buff, 0, BUFFER_SIZE);
-    int left_flag = 0;
-    while (!left_flag)
+    while (!flag)
     {
-        ssize_t n = read(sockfd, recv_buff, BUFFER_SIZE);
-        if (n < 0)
+        n = read(sockfd, recv_buff, BUFFER_SIZE);
+        if (n == -1)
         {
+            if (errno == EWOULDBLOCK || errno == EAGAIN)
+            {
+                continue;
+            }
             perror("read\t");
-            left_flag = 1;
             break;
         }
-        if (n == 0)
+        else if (n == 0)
         {
             printf("sockfd %d left\n", sockfd);
-            left_flag = 1;
             break;
         }
-        printf("%s < %d\n", recv_buff, sockfd);
-        pthread_mutex_lock(&list_mutex);
-        Node *temp = head;
-        while (temp != NULL)
+        else if (n > 0)
         {
-            if (temp->data != sockfd)
+            printf("%s < %d\n", recv_buff, sockfd);
+            pthread_mutex_lock(&list_mutex);
+            Node *temp = head;
+            while (temp != NULL)
             {
-                write(temp->data, recv_buff, strlen(recv_buff));
+                if (temp->data != sockfd)
+                {
+                    write(temp->data, recv_buff, strlen(recv_buff));
+                }
+                temp = temp->next;
             }
-            temp = temp->next;
+            pthread_mutex_unlock(&list_mutex);
+            if (!strcmp(recv_buff, "exit"))
+            {
+                break;
+            }
+            memset(recv_buff, 0, BUFFER_SIZE);
         }
-        pthread_mutex_unlock(&list_mutex);
-        if (!strcmp(recv_buff, "exit"))
-        {
-            break;
-        }
-        memset(recv_buff, 0, BUFFER_SIZE);
     }
-
     pthread_mutex_lock(&list_mutex);
     deleteNode(&head, sockfd);
     pthread_mutex_unlock(&list_mutex);
@@ -88,11 +113,13 @@ int main(int argc, char const *argv[])
     struct sockaddr_in server_addr, cli_addr;
     pthread_t client_p;
     signal(SIGINT, sig_handler);
+
     if ((server_socketFD = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
         perror("Socket\t");
         exit(EXIT_FAILURE);
     }
+    setNonblocking(server_socketFD);
     memset(&server_addr, 0, sizeof(struct sockaddr_in));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
@@ -111,25 +138,42 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
     printf("Starting Server Side Function...\n");
-
-    while (!server_flag)
+    fd_set rfds;
+    FD_ZERO(&rfds);
+    FD_SET(server_socketFD, &rfds);
+    while (!flag)
     {
-        socklen_t cli_len = sizeof(cli_addr);
-        if ((connect_FD = accept(server_socketFD, (struct sockaddr *)&cli_addr, &cli_len)) == -1)
+        int nready = select(server_socketFD + 1, &rfds, NULL, NULL, NULL);
+        if (nready <= 0)
         {
-            perror("accept\t");
             continue;
         }
-        printf("New client %d\n", connect_FD);
-
-        if ((cli_count += 1) > MAX_CLIENT)
+        if (FD_ISSET(server_socketFD, &rfds))
         {
-            printf("reach Max ...\n");
-            close(connect_FD);
-            continue;
+            socklen_t cli_len = sizeof(cli_addr);
+            connect_FD = accept(server_socketFD, (struct sockaddr *)&cli_addr, &cli_len);
+            if (connect_FD == -1)
+            {
+                perror("accept\t");
+                break;
+            }
+            else
+            {
+                printf("New client %d\n", connect_FD);
+                if ((cli_count += 1) > MAX_CLIENT)
+                {
+                    printf("reach Max ...\n");
+                    close(connect_FD);
+                    continue;
+                }
+                setNonblocking(connect_FD);
+                appendNode(&head, connect_FD);
+                pthread_create(&client_p, NULL, client_handler, &connect_FD);
+            }
         }
-        appendNode(&head, connect_FD);
-        pthread_create(&client_p, NULL, client_handler, &connect_FD);
     }
+    pthread_join(client_p, NULL);
+    close_linkedlist();
+    close(server_socketFD);
     return 0;
 }
